4c1157fd61693b76c1da876f327b60f353ea4a20
Asbel <asbel988827388@gmail.com>
2025-03-15 05:24:12 -0500

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://asbel05.github.io',
+    base: 'landing-page-restaurante-astro'
 });
